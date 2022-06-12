37d0481802e0cc34d781160e76efcffaec1a10f3
mishael dada <mishaeldada@gmail.com>
2022-06-12 21:08:38 +0100

    added project details to card component

--- src/components/Work/Cards.js
+++ src/components/Work/Cards.js
@@ -7,96 +7,40 @@ const cardItems = [
     projectThumbnail:
       "https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     previewLink: "https://mish-realestate.netlify.app/",
-    repoLink: "https://github.com/mishael1-d/realestate.git",
+    repoLink: "https://github.com/mishael1-d/realestate-website.git",
     projectDescription: "Simple real estate website, built with ReactJS.",
   },
   {
-    projectTitle: "Real Estate Website",
+    projectTitle: "Flytant Campaign Page",
     projectThumbnail:
       "https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    previewLink: "https://mish-realestate.netlify.app/",
-    repoLink: "https://github.com/mishael1-d/realestate.git",
-    projectDescription: "Simple real estate website, built with ReactJS.",
+    previewLink: "https://flytant-campaign-page.netlify.app/",
+    repoLink: "https://github.com/mishael1-d/flytant-campaignPage.git",
+    projectDescription: "Campaign page for registering of influencers",
   },
   {
-    projectTitle: "Real Estate Website",
+    projectTitle: "Flytant Notification Page",
     projectThumbnail:
       "https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    previewLink: "https://mish-realestate.netlify.app/",
-    repoLink: "https://github.com/mishael1-d/realestate.git",
-    projectDescription: "Simple real estate website, built with ReactJS.",
+    previewLink: "https://flytant-notification-page.netlify.app/",
+    repoLink: "https://github.com/mishael1-d/flytant-notificationPage.git",
+    projectDescription: "A page for clients to view/search for influencers.",
   },
   {
-    projectTitle: "Real Estate Website",
+    projectTitle: "El'mour Africa",
     projectThumbnail:
       "https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    previewLink: "https://mish-realestate.netlify.app/",
-    repoLink: "https://github.com/mishael1-d/realestate.git",
-    projectDescription: "Simple real estate website, built with ReactJS.",
+    previewLink: "https://elmour-africa.netlify.app/",
+    repoLink: "https://github.com/mishael1-d/elmour-africa.git",
+    projectDescription: "A consultancy firm agency website.",
   },
   {
-    projectTitle: "Real Estate Website",
+    projectTitle: "Bonnie's Flair",
     projectThumbnail:
       "https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     previewLink: "https://mish-realestate.netlify.app/",
-    repoLink: "https://github.com/mishael1-d/realestate.git",
-    projectDescription: "Simple real estate website, built with ReactJS.",
-  },
-  {
-    projectTitle: "Real Estate Website",
-    projectThumbnail:
-      "https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    previewLink: "https://mish-realestate.netlify.app/",
-    repoLink: "https://github.com/mishael1-d/realestate.git",
-    projectDescription: "Simple real estate website, built with ReactJS.",
-  },
-  {
-    projectTitle: "Real Estate Website",
-    projectThumbnail:
-      "https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    previewLink: "https://mish-realestate.netlify.app/",
-    repoLink: "https://github.com/mishael1-d/realestate.git",
-    projectDescription: "Simple real estate website, built with ReactJS.",
-  },
-  {
-    projectTitle: "Real Estate Website",
-    projectThumbnail:
-      "https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    previewLink: "https://mish-realestate.netlify.app/",
-    repoLink: "https://github.com/mishael1-d/realestate.git",
-    projectDescription: "Simple real estate website, built with ReactJS.",
-  },
-  {
-    projectTitle: "Real Estate Website",
-    projectThumbnail:
-      "https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    previewLink: "https://mish-realestate.netlify.app/",
-    repoLink: "https://github.com/mishael1-d/realestate.git",
-    projectDescription: "Simple real estate website, built with ReactJS.",
-  },
-  {
-    projectTitle: "Real Estate Website",
-    projectThumbnail:
-      "https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    previewLink: "https://mish-realestate.netlify.app/",
-    repoLink: "https://github.com/mishael1-d/realestate.git",
-    projectDescription: "Simple real estate website, built with ReactJS.",
-  },
-  {
-    projectTitle: "Real Estate Website",
-    projectThumbnail:
-      "https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    previewLink: "https://mish-realestate.netlify.app/",
-    repoLink: "https://github.com/mishael1-d/realestate.git",
-    projectDescription: "Simple real estate website, built with ReactJS.",
-  },
-  {
-    projectTitle: "Real Estate Website",
-    projectThumbnail:
-      "https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    previewLink: "https://mish-realestate.netlify.app/",
-    repoLink: "https://github.com/mishael1-d/realestate.git",
-    projectDescription: "Simple real estate website, built with ReactJS.",
+    repoLink: "https://github.com/mishael1-d/bonnie-flair.git",
+    projectDescription: "An e-commerce website for selling glasses, bags etc.",
   },
 ];
 function Cards() {
