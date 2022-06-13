b2eceb8e63d8bc686c8b457233d97b115e738dd0
mishael dada <mishaeldada@gmail.com>
2022-06-13 18:26:47 +0100

    contact page intial commit

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,10 +1,23 @@
-import React from 'react'
-import './Contact.css'
+import React from "react";
+import "./Contact.css";
 
 function Contact() {
   return (
-    <div className='container'>Contact</div>
-  )
+    <div className="contact-section" id="contact">
+      <div className="container">
+        <div className="section-text__header">
+          <h3 className="section-text__title">Let's Connect</h3>
+          <div className="divider" />
+        </div>
+        <div className="social-media links-container"></div>
+        <div className="form-container">
+          <form>
+
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
